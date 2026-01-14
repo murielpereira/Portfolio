0590571452336e5a6afe9ae5faceeeb459787f15
Muriel <murielpereirabr@gmail.com>
2026-01-14 20:33:26 -0300

    Minhas novas atualizacoes

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,3 +1,9 @@
+fetch('../header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+})
+
 function soarClick() {
     var click = new Audio('sons/click.mp3')
 
@@ -36,4 +42,44 @@ function calcular(tipo, valor) {
         var valor_campo = document.getElementById('resultado').value
         document.getElementById('resultado').value += valor
     }
+}
+
+var contadorCliques = 0;
+var timer; 
+
+function verificarEasterEgg() {
+    clearTimeout(timer);
+    contadorCliques++;
+
+    console.log("Cliques: " + contadorCliques);
+
+    if (contadorCliques === 2) {
+        exibirPopup();
+        contadorCliques = 0;
+    }
+
+    timer = setTimeout(function() {
+        contadorCliques = 0;
+        console.log("Tempo esgotado, contador resetado.");
+    }, 2000); 
+}
+
+function exibirPopup() {
+    var popup = document.getElementById('easterEggPopup');
+    popup.classList.remove('popup-oculto');
+    popup.classList.add('popup-visivel');
+
+    var nazare = new Audio('sons/nazare.mp3')
+    nazare.currentTime = 5
+    nazare.play()
+
+    setTimeout(function() {
+        nazare.pause();
+    }, 2000);
+}
+
+function fecharPopup() {
+    var popup = document.getElementById('easterEggPopup');
+    popup.classList.remove('popup-visivel');
+    popup.classList.add('popup-oculto');
 }
